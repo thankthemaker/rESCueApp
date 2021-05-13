22b07406ea23656045f4078305001534496cc825
dgey <david.gey@thank-the-maker.org>
2021-05-13 20:26:56 +0200

    changed name

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -14,7 +14,12 @@ export class HomePage {
   scan() {
     try {
       BleClient.initialize();
-      BleClient.requestDevice();
+      BleClient.requestDevice({
+        //services: [ 'd804b643-6ce7-4e81-9f8a-ce0f699085eb', '713d0000-503e-4c75-ba94-3148f18d941e' ]
+        services: [ '6e400001-b5a3-f393-e0a9-e50e24dcca9e' ]
+      }).then((device) => {
+        console.log('connected to device ' + device.name + '(' + atob(device.deviceId) + ')');
+      });
     } catch (error) {
       console.error(error);
     }
