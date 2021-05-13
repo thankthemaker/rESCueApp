e56df0da8b8699d84e03f909ac54f4cf0adf8bcc
dgey <david.gey@thank-the-maker.org>
2021-05-13 19:34:47 +0200

    switched to capacitor and webcomponents

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BleClient, numberToUUID } from '@capacitor-community/bluetooth-le';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,15 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor() {
+  }
 
+  scan() {
+    try {
+      BleClient.initialize();
+      BleClient.requestDevice();
+    } catch (error) {
+      console.error(error);
+    }
+  }
 }
