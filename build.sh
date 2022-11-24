bd6c59fcc6fd5374708494466623748c619a2fd7
dgey <david.gey@thank-the-maker.org>
2022-11-24 18:06:24 +0100

    JSON KEY

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ ionic build --prod --aot --output-hashing=all && npx cap sync
 
 if [[ "$os" == "android" ]]; then
  cd android/
- fastlane internal
+ fastlane android internal
  cd -
 fi
 
@@ -20,7 +20,7 @@ if [[ "$os" == "ios" ]]; then
   export FASTLANE_ITUNES_TRANSPORTER_USE_SHELL_SCRIPT=1
   export FASTLANE_ITUNES_TRANSPORTER_PATH=/Applications/Transporter.app/Contents/itms
  cd ios/App/fastlane
- fastlane beta
+ fastlane ios beta
  cd -
 fi
 
