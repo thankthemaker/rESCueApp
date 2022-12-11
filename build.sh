602eb89ce916b3418192353fe29d5314201ed398
dgey <david.gey@thank-the-maker.org>
2022-12-11 14:30:14 +0100

    Optimize build pipeline

--- build.sh
+++ build.sh
@@ -3,14 +3,22 @@
 os=$1
 type=$2
 
+#npm install
+npx cap add android
+#npx cap add ios
+
+npx trapeze run trapeze-config.yaml -y
+#npx capacitor-assets generate --iconBackgroundColor '#000000' --iconBackgroundColorDark '#000000' --splashBackgroundColor '#000000' --splashBackgroundColorDark '#000000' --logoSplashTargetWidth 256
+
+ionic build --prod --aot --output-hashing=all && npx cap sync
+
+ln -snf ~/.fastlane_env fastlane/.env.default
+
 if [[ "x${os}x" == "xx" ]]; then
-  echo "missing os, either android or ios"
+  echo "missing os, either android or ios is required to build an app"
   exit -1
 fi
 
-#ionic build --prod --aot --output-hashing=all && npx cap sync
-
-ln -snf ~/.fastlane_env fastlane/.env.default
 
 if [[ "$os" == "android" ]]; then
  find node_modules -type f -name '*.gradle' -exec sed -i '' 's/compile /implementation /g' {} \;
@@ -23,6 +31,6 @@ fi
 if [[ "$os" == "ios" ]]; then
   export FASTLANE_ITUNES_TRANSPORTER_USE_SHELL_SCRIPT=1
   export FASTLANE_ITUNES_TRANSPORTER_PATH=/Applications/Transporter.app/Contents/itms
- fastlane ios beta
+  fastlane ios beta
 fi
 
